507c9cac57b5a70c2ba40acaf119dcd22a65362d
Guilherme Willian de Oliveira <gwo.glt@scopus.com.br>
2016-03-11 14:50:26 -0300

    implementando pensador profundo

--- src/main/java/me/impressione/cucumberdemo/PensadorProfundo.java
+++ src/main/java/me/impressione/cucumberdemo/PensadorProfundo.java
@@ -9,14 +9,34 @@ package me.impressione.cucumberdemo;
  */
 public class PensadorProfundo {
 
+    /**
+     * 
+     */
+    private static final Double TEMPO_PROCESSAMENTO = new Double("7.5");
+    private boolean processandoPerguntaFundamental;
+    private Double tempoProcessando = 0.0;
+
     public void processarPerguntaFundamental() {
-        
+        this.processandoPerguntaFundamental = true;
     }
+
     public void tempoAguardando(Double anosEmMilhoes) {
-        
+        if (processandoPerguntaFundamental) {
+            this.tempoProcessando = anosEmMilhoes;
+        }
     }
 
     public String resposta() {
-        return null;
+        if (fimProcessamento()) {
+            return "42";
+        } else if (processandoPerguntaFundamental) {
+            return "ainda pensando";
+        } else {
+            return "primeiro você deve fazer a pergunta";
+        }
+    }
+
+    private boolean fimProcessamento() {
+        return TEMPO_PROCESSAMENTO.compareTo(tempoProcessando) < 0;
     }
 }
