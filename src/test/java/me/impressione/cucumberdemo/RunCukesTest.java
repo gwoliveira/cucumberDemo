d750909741e96663f1ae8ffc3e3e17ebf24db8d9
Guilherme Willian de Oliveira <gwo.glt@scopus.com.br>
2016-03-11 15:09:15 -0300

    relatorio cucumber html

--- src/test/java/me/impressione/cucumberdemo/RunCukesTest.java
+++ src/test/java/me/impressione/cucumberdemo/RunCukesTest.java
@@ -12,6 +12,6 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(monochrome = true, features="classpath:funcionalidades", plugin = {"pretty"})
+@CucumberOptions(monochrome = true, features="classpath:funcionalidades", plugin = {"pretty", "html:target/cucumber"})
 public class RunCukesTest {  
 }
