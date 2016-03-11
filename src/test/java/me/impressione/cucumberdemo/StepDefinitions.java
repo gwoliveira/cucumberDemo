f3991b72508c7f212a5208e0024fe426e2f2ab69
Guilherme Willian de Oliveira <gwo.glt@scopus.com.br>
2016-03-11 14:32:57 -0300

    TDD criando chamadas para o PensadorProfundo

--- src/test/java/me/impressione/cucumberdemo/StepDefinitions.java
+++ src/test/java/me/impressione/cucumberdemo/StepDefinitions.java
@@ -3,7 +3,9 @@
  */
 package me.impressione.cucumberdemo;
 
-import cucumber.api.PendingException;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 import cucumber.api.java8.Pt;
 
 /**
@@ -11,25 +13,26 @@ import cucumber.api.java8.Pt;
  *
  */
 public class StepDefinitions implements Pt {
+    
+    private PensadorProfundo pensadorProfundo;
+    
     public StepDefinitions() {
+        
+        
         Dado("^o Pensador Profundo$", () -> {
-            // Write code here that turns the phrase above into concrete actions
-            throw new PendingException();
+            pensadorProfundo = new PensadorProfundo();
         });
 
         Quando("^perguntado sobre a resposta pra vida, o universo, e tudo mais$", () -> {
-            // Write code here that turns the phrase above into concrete actions
-            throw new PendingException();
+            pensadorProfundo.processarPerguntaFundamental();
         });
 
-        Então("^a resposta é \"([^\"]*)\"$", (String arg1) -> {
-            // Write code here that turns the phrase above into concrete actions
-            throw new PendingException();
+        Então("^a resposta é \"([^\"]*)\"$", (String respostaEsperada) -> {
+            assertThat(respostaEsperada, is(pensadorProfundo.resposta()));
         });
 
         E("^após passar ([\\d\\.]+) milhões de anos$", (Double anosEmMilhoes) -> {
-            // Write code here that turns the phrase above into concrete actions
-            throw new PendingException();
+            pensadorProfundo.tempoAguardando(anosEmMilhoes);
         });
     }
 }
