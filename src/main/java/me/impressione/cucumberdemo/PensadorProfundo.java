769353cd0f95c4794ff7686f3191e85081e66340
Guilherme Willian de Oliveira <gwo.glt@scopus.com.br>
2016-03-11 14:34:10 -0300

    TDD gerando esqueleto da classe PensadorProfundo

---
+++ src/main/java/me/impressione/cucumberdemo/PensadorProfundo.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package me.impressione.cucumberdemo;
+
+/**
+ * @author Guilherme Willian de Oliveira
+ *
+ */
+public class PensadorProfundo {
+
+    public void processarPerguntaFundamental() {
+        
+    }
+    public void tempoAguardando(Double anosEmMilhoes) {
+        
+    }
+
+    public String resposta() {
+        return null;
+    }
+}
